d0d5f0c99344932eade69b73c5c92c756031d415
Arturo <cosasscosas@gmail.com>
2022-06-09 17:55:50 +0200

    primer commit inicial

---
+++ data.js
@@ -0,0 +1,172 @@
+/* Algún dia usare el asincrono */
+
+  // const jsontest = fetch('./sites.json')
+  // .then(response =>{ return response.json()})
+  // .then(data => console.log(data))
+  // .catch(error => console.log(error)); 
+  // console.log(jsontest);
+const jsontest = {
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.403421,-3.678291]
+			
+		},
+		"properties": {
+            "Name": "Kebab Omar \n",
+			"Calle": " Calle de Juan de Urbieta, 2, 28007 Madrid\n",
+			"calidad": "five",
+            "descripcion": "Situado en un barrio cercano a Atocha, uno de mis lugares favoritos en calidad/precio,con un buen trato pan bien crujiente y un buen, el sabor de ambas salsas estan muy buenas (salsa roja no picante) y económico. Menú kebab por 5.50€",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.345198,-3.520722]
+			
+		},
+		"properties": {
+            "Name": "Ali kebab \n",
+			"Calle": "C/ Luis Mateo Díez, 1, 28521 Rivas-Vaciamadrid, Madrid, España\n",
+			"calidad": "five",
+			"descripcion": "Aunque pilla algo lejos, tiene una gran calidad/precio, con un pan bien crujiente, el sabor de ambas salsas estan muy buenas (salsa roja no picante) y económico. Menú kebab por 5.50€",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.387618,-3.64124]
+			
+		},
+		"properties": {
+            "Name": "Ateshgah\n",
+			"Calle": "C/ Luis Mateo Díez, 1, 28521 Rivas-Vaciamadrid, Madrid, España\n",
+			"calidad": "four",
+			"descripcion": "En comparación es bastante majo de precio, depende del día te lo pueden dar con un pan crujiente, el sabor de la carne y la salsa esta muy bien. Menú kebab por 6.50€",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.387618,-3.64124]
+			
+		},
+		"properties": {
+            "Name": "Jalo \n",
+			"Calle": "C. de Atocha, 116, 28012 Madrid\n",
+			"calidad": "four",
+			"descripcion": "De los más conocidos por el centro, tiene una gran variedad de platos, pan normalito pero el sabor de la carne y la salsa no está mal.",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.432956,-3.717391]
+			
+		},
+		"properties": {
+            "Name": "Kebab house \n",
+			"Calle": "C. de Meléndez Valdés, 67, 28015 Madrid\n",
+			"calidad": "four",
+			"descripcion": "Cierra prontisimo, si consigues uno verás que el sabor de la carne esta muy muy bien, ideal para pedir con un durum",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.574298,-4.004451]
+			
+		},
+		"properties": {
+            "Name": "Kebabish Turco \n",
+			"Calle": "C. de los Alamillos, 28260 Galapagar, Madrid\n",
+			"calidad": "four",
+			"descripcion": "un kebab que recuerdo que la carne tenía un toque picante, pan normalito, patatas normales",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.394271,-3.661116]
+			
+		},
+		"properties": {
+            "Name": "kebab sr bangla\n",
+			"Calle": "Av. de la Albufera, 97, 28038 Madrid\n",
+			"calidad": "three",
+			"descripcion": "De las opciones más baratas que puedes encontrar, es un kebab normalito en el que no destacarías gran cosa",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.406442,-3.676364]
+			
+		},
+		"properties": {
+            "Name": "Kebab madrid \n",
+			"Calle": " C. de Cavanilles, 17, 28007\n",
+			"calidad": "two-one",
+			"descripcion": "Pan normalito, no destacaría apenas el sabor de la carne, apenas te lo dan con algo de salsa",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.398939,-3.661979]
+			
+		},
+		"properties": {
+            "Name": "Kebab Hut \n",
+			"Calle": "C. del Puerto Canfranc, 41, 28038 Madrid, España\n",
+			"calidad": "two-one",
+			"descripcion": "Pan normal,la carne con algo de sabor, kebab con poca salsa y las patatas con demasiada, lo malo es que estaban crudas.",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [40.403399,-3.60869]
+			
+		},
+		"properties": {
+            "Name": "Kebabish Vicálvaro \n",
+			"Calle": "C. Lago Sanabria, 2, 28032 Madrid\n",
+			"calidad": "two-one",
+			"descripcion": "Regulero, nada destacable, pan blando y carne que no llama la atención",
+			"notes": "",
+         "icon": "mediumIcon"
+		}
+	}
+]
+};
